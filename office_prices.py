43b4740d409cd01ff853404ffa8e23ac603c0bae
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2024-02-09 13:52:16 +0530

    Added and tested code to train and use a simple Polynomial regression model of 3 degrees

--- office_prices.py
+++ office_prices.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 """
-This module is my attempt to solve the Bot Saves Princess problem 
+This module is my attempt to solve the problem 
 (https://www.hackerrank.com/challenges/predicting-office-space-price/problem?isFullScreen=true)
- on HackerRank. 
-
+ on HackerRank. Also works (w/o the polynomial features part) for the problem
+ https://www.hackerrank.com/challenges/predicting-house-prices/problem?isFullScreen=true
+    
 Author: Deekshith Rao
 Date: 2024-02-09
 """
 import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
 
 def read_input(f):
@@ -22,3 +25,22 @@ T, = read_input(int)
 test = pd.DataFrame(columns=list(range(1, F+1)), dtype=float)
 for row in range(T):
     test.loc[row] = read_input(float)
+
+
+# Separate features and target variables
+X_train = train.iloc[:, :-1]
+y_train = train.iloc[:, -1]
+
+# Create polynomial features
+poly = PolynomialFeatures(degree=3)
+X_train_poly = poly.fit_transform(X_train)
+
+# Fit polynomial regression model
+model = LinearRegression()
+model.fit(X_train_poly, y_train)
+
+# Predict on test data
+X_test_poly = poly.transform(test)
+predictions = model.predict(X_test_poly)
+
+print('\n'.join(map(str, predictions)))
