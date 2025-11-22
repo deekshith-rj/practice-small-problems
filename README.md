eea55c0c813d3ac1b4be12fed2559dcbf3f91174
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2025-11-23 03:36:14 +0530

    predict_wellness.py :
    Refactored wellness prediction model to include scaling, PCA, and hyperparameter tuning
    
    - Updated the model to use a pipeline with StandardScaler and PCA for preprocessing.
    - Replaced individual model instantiations with a Ridge regression model in a pipeline.
    - Implemented GridSearchCV for hyperparameter tuning of the Ridge model's alpha parameter.
    - Updated the Kaggle competition link and modified the date in the module docstring.
    - Adjusted the accuracy print statements to reflect the best score from the grid search.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # small-problems
+
+This repo is for my practice on small problems related to Data Science, Machine Learning & Deep Learning.
+More involved problems are include in https://github.com/deekshith-rj/practice
--- predict_wellness/predict_wellness.py
+++ predict_wellness/predict_wellness.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 """
 This module is my attempt to solve the problem 
-(https://www.hackerrank.com/challenges/stockprediction?isFullScreen=true)
- on HackerRank.
+(https://www.kaggle.com/competitions/ml-olympiad-predicting-wellness)
+ on Kaggle.
     
 Author: Deekshith Rao
-Date: 2024-02-09
+Date: 2024-04-06
 """
 
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
 
 # Load the data
 train = pd.read_csv('./data/bodyfat-comp.csv')
@@ -19,11 +23,24 @@ X_train, y_train = train.drop('BodyFat', axis=1), train['BodyFat']
 test = pd.read_csv('./data/bodyfat-validate.csv')
 
 # Fit the model
-model = LinearRegression()
-model = Ridge(alpha=0.5)
-model = Lasso(alpha=0.7)
+scaler = StandardScaler()
+pca = PCA()
+# model = LinearRegression()
+model = Ridge()
+# model = Lasso(alpha=0.7)
+pipe = Pipeline([('scaler', scaler), ('pca', pca), ('model', model)])
+param_grid = {
+    # 'pca__n_components': [2, 4, 6, 8, 10, 12, 13], 
+    # 'model__alpha': np.linspace(0.005, 0.5, 100)
+    'model__alpha': [0.25, 0.5, 0.75, 1, 1.25, 1.5, 2, 2.5, 3, 4, 5, 7.5, 10]
+}
+search = GridSearchCV(pipe, param_grid=param_grid, 
+                      scoring='neg_mean_squared_error', verbose=True)
+search.fit(X_train, y_train)
 scores = cross_val_score(model, X_train, y_train, cv=5, scoring='neg_mean_squared_error')
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+print("Accuracy: " + str(search.best_score_))
+
 
 # Predict the test data
 model.fit(X_train, y_train)
